edaf2798faf1ad7045b6ff675959899a09a6c5a5
Mehul Behera <mbehera@umd.edu>
2024-06-24 14:13:34 -0400

    finished selection sort

--- src/algorithms/selectionSort.ts
+++ src/algorithms/selectionSort.ts
@@ -1,5 +1,37 @@
-let initArray = [6, 3, 4, 8, 9, 1];
+export function selectionSortStep(
+  toSortArray: number[],
+  numSorted: number
+): number[] {
+  const arrayToReturn = [...toSortArray];
+
+  if (numSorted > arrayToReturn.length - 2) {
+    return toSortArray;
+  }
+
+  let minIndex = -1;
+  let minValue = Infinity;
+  for (let i = numSorted; i < arrayToReturn.length; i++) {
+    //find the smallest value
+    if (arrayToReturn[i] < minValue) {
+      minIndex = i;
+      minValue = arrayToReturn[i];
+    }
+  }
+
+  //switch it with numSorted
+  const tempValue = arrayToReturn[numSorted];
+  arrayToReturn[numSorted] = minValue;
+  arrayToReturn[minIndex] = tempValue;
+
+  return arrayToReturn;
+}
 
 export default function selectionSort(toSortArray: number[]): number[] {
+  let numSorted = 0;
+  while (numSorted < toSortArray.length) {
+    toSortArray = selectionSortStep(toSortArray, numSorted);
+    numSorted++;
+  }
+
   return toSortArray;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import bubbleSort, { bubbleSortStep } from "@/algorithms/bubbleSort";
 import BubbleSortComponent from "@/components/BubbleSortComponent";
 import Navigation from "@/components/Navigation";
+import SelectionSortComponent from "@/components/SelectionSortComponent";
 import { useState } from "react";
 
 let initArray = [6, 3, 4, 8, 9, 1];
@@ -11,9 +12,11 @@ export default function Home() {
   return (
     <div>
       <Navigation />
+      <SelectionSortComponent initArray={initArray} />
     </div>
   );
 }
 
 
 //<BubbleSortComponent initArray={initArray} />
+//<SelectionSortComponent initArray={initArray} />
